377c4c2a4f85a6852449ceb1fbc616dd36bd5e66
Matt Trussell <smtrussell@gmail.com>
2018-04-29 20:34:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,13 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team = params[:team]
-
-      @heroes = []
-      params[:team][:heroes].each do |hero_details|
-        hero = Hero.new(hero_details)
-        @heroes << hero
-      end
+      # @team = params[:team]
+      # 
+      # @heroes = []
+      # params[:team][:heroes].each do |hero_details|
+      #   hero = Hero.new(hero_details)
+      #   @heroes << hero
+      # end
 
       erb :team
     end
